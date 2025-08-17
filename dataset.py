06c4ef1df2ac206d9711101e54c5aad09492b309
local_tweekerz <94663534+LocalTweekez@users.noreply.github.com>
2025-08-17 18:59:34 +0200

    data
    data

---
+++ dataset.py
@@ -0,0 +1,7 @@
+import kagglehub
+
+path1 = kagglehub.dataset_download("snehaanbhawal/resume-dataset")
+path2 = kagglehub.dataset_download("maitrip/resumes")
+
+print("Path to dataset files:", path1) 
+print("Path to dataset files:", path2) 
